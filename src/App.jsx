460b37922b5921632d3986b70f002f564408d318
Meghana <meghanamanchal@gmail.com>
2024-02-01 15:38:44 +0530

    used useMemo and useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState,useMemo,useCallback } from 'react';
 import './App.css';
 
 // Do not change this
@@ -15,69 +15,69 @@ function App() {
 
 
   // should not change the LOGIC inside this function - you can make changes to the function but logic should NOT change
-  const delayFunction = ()=> {
+  const delayFunction = useMemo(() => {
     console.log("Delay Function Ran")
-    for(let index=0; index<LARGE_NUMBER; index++){};
-    return value+2;
-  
-  }
+    for (let index = 0; index < LARGE_NUMBER; index++) { };
+    return value + 2;
+
+  },[value])
 
   // should not change the LOGIC inside this function - you can make changes to the function but logic should NOT change
-  const testFunction = ()=>{
-    return [value*3 ,value*4]
-  }
+  const testFunction = useCallback(() => {
+    return [value * 3, value * 4]
+  },[value])
 
-  
+
 
   // should not change this
-  useEffect(()=>{
+  useEffect(() => {
     console.log("Callback Function was called")
-  },[testFunction])
+  }, [testFunction])
 
 
 
-  useEffect(()=>{
-    if(dark){
+  useEffect(() => {
+    if (dark) {
       setThemeName("dark")
     }
-    else{
+    else {
       setThemeName("light")
     }
-  },[dark])
+  }, [dark])
 
 
-  const handleClick = ()=>{
+  const handleClick = () => {
     setTheme(!dark);
   }
 
-  const handleChangeValue = ()=>{
-    setValue(value+1);
+  const handleChangeValue = () => {
+    setValue(value + 1);
   }
 
-  const handleList = ()=>{
+  const handleList = () => {
     setList(testFunction);
   }
 
   const styleTheme = {
-   backgroundColor:dark ? "black":"#ccc7c7",
+    backgroundColor: dark ? "black" : "#ccc7c7",
   }
 
   return (
-    
+
     <div className="page" style={styleTheme}>
       <button onClick={handleClick}>{themeName}</button>
       <h1 >{value}</h1>
       <button onClick={handleChangeValue}>Change Value</button>
       <button onClick={handleList}>Show List</button>
-      <h2>{delayFunction()}</h2>
+      <h2>{delayFunction}</h2>
       <div>
-        {currentList.map((item,index)=>{
+        {currentList.map((item, index) => {
           return <h2 key={index}>{item}</h2>
         })}
 
       </div>
     </div>
-    
+
   );
 }
 
